a26b0ff70a2eb43800ae286384e72f3210969aab
IKYNCHI <kynchsilao@gmail.com>
2022-06-06 21:28:04 +0800

    updated

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -18,7 +18,7 @@ const Dashboard = () => {
             </Link>
         </div>
         <ul className='nav-items'>
-        <li className='nav-item'><Link to='/dashboard'>Dashboard</Link></li>
+        <li className='nav-item'><a href='https://dakboard.com/app/screenPredefined?p=a911d765329754a6321b3257a37e1583'>Dashboard</a></li>
         <li className='nav-item'><Link to='/viewdevices'>View Devices</Link></li>
         <li className='nav-item'><Link to='/'>Logout</Link></li>
         </ul>
